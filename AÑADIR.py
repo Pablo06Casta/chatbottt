a93e96e44fd43518d0389b91c5c38f4fe3ca8725
Pablo <pablitocastaneda06@icloud.com>
2024-06-28 23:33:33 +0200

    Create AÑADIR.py

---
+++ AÑADIR.py
@@ -0,0 +1,11 @@
+"""intalar librerias
+  import streamlit as st
+  import random
+  import json
+  import pickle
+  import numpy as np
+  import nltk
+  from nltk.stem import WordNetLemmatizer
+  from keras.models import load_model # type: ignore"""
+
+#Hacer entorno virtual
